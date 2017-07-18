566867939870076ac1ff4face3a2f2ee31bfed0e
Fraser Sprott <frasers@hotmail.co.uk>
2017-07-18 16:45:23 +0100

    reduced the amount of magic numbers

--- src/main/scala/Notation.scala
+++ src/main/scala/Notation.scala
@@ -12,21 +12,22 @@ class Notation {
     }
 
     def convertNumber(number :BigInt, useShortNotation: Boolean): String ={
-      def iter(numberString: String, notation :List[String]): String = numberString.length match{
-        case 0 => ""
-        case 4 => numberString.take(1) + notation(0) + iter(numberString.tail, notation)
-        case 7 => numberString.take(1) + notation(1) + iter(numberString.tail, notation)
-        case 10 => numberString.take(1) + notation(2) + iter(numberString.tail, notation)
-        case 13 => numberString.take(1) + notation(3) + iter(numberString.tail, notation)
-        case 16 => numberString.take(1) + notation(4) + iter(numberString.tail, notation)
-        case 19 => numberString.take(1) + notation(5) + iter(numberString.tail, notation)
-        case 22 => numberString.take(1) + notation(6) + iter(numberString.tail, notation)
+      def iter(numberString: String, notation :List[String]): String = numberString.length.toString match{
+        case "0" => ""
+        case "4" => numberString.take(1) + notation(0) + iter(numberString.tail, notation)
+        case "7" => numberString.take(1) + notation(1) + iter(numberString.tail, notation)
+        case "10" => numberString.take(1) + notation(2) + iter(numberString.tail, notation)
+        case "13" => numberString.take(1) + notation(3) + iter(numberString.tail, notation)
+        case "16" => numberString.take(1) + notation(4) + iter(numberString.tail, notation)
+        case "19" => numberString.take(1) + notation(5) + iter(numberString.tail, notation)
+        case "22" => numberString.take(1) + notation(6) + iter(numberString.tail, notation)
         case _ =>  numberString.take(1) + iter(numberString.tail, notation)
       }
       iter(number.toString(), getTerms(useShortNotation))
     }
 
     def display(number :Long): Unit ={
+
       println("Short Notation:\n" + convertNumber(number, useShort))
       println("Long Notation:\n" + convertNumber(number, useLong))
     }
