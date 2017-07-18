f7807fced648070b5abd73f0ca92660c5101a460
Fraser Sprott <frasers@hotmail.co.uk>
2017-07-18 16:53:02 +0100

    got rid of the magic numbers in the convertNumber function

--- src/main/scala/Notation.scala
+++ src/main/scala/Notation.scala
@@ -1,7 +1,15 @@
 
 class Notation {
-    val useShort = true
-    val useLong = false
+  val useShort = true
+  val useLong = false
+
+  val thousand = 0
+  val million = 1
+  val billion = 2
+  val trillion = 3
+  val quadrillion = 4
+  val quintillion = 5
+  val sextillion = 6
 
     def getTerms(useShort: Boolean):List[String]={
       if(useShort) {
@@ -14,13 +22,13 @@ class Notation {
     def convertNumber(number :BigInt, useShortNotation: Boolean): String ={
       def iter(numberString: String, notation :List[String]): String = numberString.length.toString match{
         case "0" => ""
-        case "4" => numberString.take(1) + notation(0) + iter(numberString.tail, notation)
-        case "7" => numberString.take(1) + notation(1) + iter(numberString.tail, notation)
-        case "10" => numberString.take(1) + notation(2) + iter(numberString.tail, notation)
-        case "13" => numberString.take(1) + notation(3) + iter(numberString.tail, notation)
-        case "16" => numberString.take(1) + notation(4) + iter(numberString.tail, notation)
-        case "19" => numberString.take(1) + notation(5) + iter(numberString.tail, notation)
-        case "22" => numberString.take(1) + notation(6) + iter(numberString.tail, notation)
+        case "4" => numberString.take(1) + notation(thousand) + iter(numberString.tail, notation)
+        case "7" => numberString.take(1) + notation(million) + iter(numberString.tail, notation)
+        case "10" => numberString.take(1) + notation(billion) + iter(numberString.tail, notation)
+        case "13" => numberString.take(1) + notation(trillion) + iter(numberString.tail, notation)
+        case "16" => numberString.take(1) + notation(quadrillion) + iter(numberString.tail, notation)
+        case "19" => numberString.take(1) + notation(quintillion) + iter(numberString.tail, notation)
+        case "22" => numberString.take(1) + notation(sextillion) + iter(numberString.tail, notation)
         case _ =>  numberString.take(1) + iter(numberString.tail, notation)
       }
       iter(number.toString(), getTerms(useShortNotation))
